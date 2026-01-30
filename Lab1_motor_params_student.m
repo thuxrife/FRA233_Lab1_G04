9c9f8181cae0598340e5c01f7c0003ed53d62ef9
LaGooNcon <songkran50507@gmail.com>
2026-01-31 02:18:22 +0700

    Adjustable Files

--- Lab1_motor_params_student.m
+++ Lab1_motor_params_student.m
@@ -9,15 +9,15 @@ This script for prepare data and parameters for parameter estimator.
 motor_R = 10.30982581;
 motor_L = 35.0259456;
 % Optimization's parameters
-motor_Eff = 0.438057;
-motor_Ke = 0.0767294;
-motor_J = 0.001;
-motor_B = 0.0001;
+motor_Eff = 0.96157;
+motor_Ke = 0.074986;
+motor_J = 1e-09;
+motor_B = 1.11e-05;
 
 % Extract collected data
-Input = data{1}.Values.Data;
-Time = data{1}.Values.Time;
-Velo = double(data{1}.Values.Data);
+Input = out.data.Data;
+Time = out.data.Time;
+Velo = double(out.data.Data);
 
 % Plot 
 figure(Name='Motor velocity response')
